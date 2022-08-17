fac74d71a5778d6dc10240922ea486b098dff15d
BellaNguyen573 <111455079+BellaNguyen573@users.noreply.github.com>
2022-08-18 01:04:49 +0700

    Add files via upload
    Upload files

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Đinh Ngân';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy birthday';
+const titleCard = 'Tặng Bae <3';
+const contentCard = 'Happy Birthday to my darling! <br>Chúc em sinh nhật vui vẻ, hạnh phúc, tràn đầy sức khỏe và thành công trên con đường mà em đã lựa chọn.<br> Cảm ơn em đã luôn cố gắng và đồng hành cùng bồ trong những năm tháng thanh xuân tươi đẹp.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
